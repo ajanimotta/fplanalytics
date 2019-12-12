bc7942f20a68ecf976cd83e66a9e7a2b92134ee7
Ajani Motta <amotta11003@yahoo.com>
2019-12-12 15:43:11 -0500

    add wsgi for heroku deployment

---
+++ fpl_django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fpl_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fpl_django.settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
