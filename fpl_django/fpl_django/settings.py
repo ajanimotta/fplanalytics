eb0f0c994323c5001476913ce3cf81611f955dbf
Ajani Motta <amotta11003@yahoo.com>
2019-12-12 15:16:57 -0500

    fix django-heroku import

--- fpl_django/fpl_django/settings.py
+++ fpl_django/fpl_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django-heroku
+import django_heroku
 from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
