47fc2960cbde68bed3360119f0880de10d4947a9
Ajani Motta <amotta11003@yahoo.com>
2019-12-13 14:26:23 -0500

    edit wsgi and Procfile

--- fpl_django/fpl_django/wsgi.py
+++ fpl_django/fpl_django/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fpl_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fpl_django.fpl_django.settings')
 
 application = get_wsgi_application()
